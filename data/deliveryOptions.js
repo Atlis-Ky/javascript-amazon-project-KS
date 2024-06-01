ce75fa7367089fb6cb441259d2c2116f7625b7d6
kai.dev <kylesmith97@outlook.com>
2024-06-01 21:11:52 +0100

    moved order summary into its own file

--- data/deliveryOptions.js
+++ data/deliveryOptions.js
@@ -1,5 +1,5 @@
 export const deliveryOptions = [{
-  id: '1', 
+  id: '1',
   deliveryDays: 7,
   priceCents: 0
 }, {
@@ -11,3 +11,15 @@ export const deliveryOptions = [{
   deliveryDays: 1,
   priceCents: 999
 }];
+
+export function getDeliveryOption(deliveryOptionId) {
+  let deliveryOption;
+
+  deliveryOptions.forEach((option) => {
+    if (option.id === deliveryOptionId) {
+      deliveryOption = option;
+    }
+  });
+
+  return deliveryOption || deliveryOptions[0];
+}
