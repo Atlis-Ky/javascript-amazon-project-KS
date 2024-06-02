1d1e82556a6f7d07422583dca98d6d571ac4e956
kai.dev <kylesmith97@outlook.com>
2024-06-02 16:08:57 +0100

    Add jasmine testing framework

---
+++ scripts/utils/tests/moneyTest.js
@@ -0,0 +1,32 @@
+import {formatCurrency} from '../money.js';
+
+console.log('test suite: fomratCurrency');
+
+console.log('converts cents into dollars');
+if (formatCurrency(2095) === '20.95') {
+  console.log('passed');
+} else {
+  console.log('failed');
+}
+
+console.log('works with 0 value');
+if (formatCurrency(0) === '0.00') {
+  console.log('passed');
+} else {
+  console.log('failed');
+}
+
+console.log('round up to the nearest cent');
+if (formatCurrency(2000.5) === '20.01') {
+  console.log('passed');
+} else {
+  console.log('failed');
+}
+
+
+console.log('rounds down to nearest cent');
+if (formatCurrency(2000.4) === '20.00') {
+  console.log('passed');
+} else {
+  console.log('failed');
+}
