3efcb54659fbcdb2dbaeb17add2017311417f8d8
kai.dev <kylesmith97@outlook.com>
2024-06-11 11:23:33 +0100

    learn error handling

--- data/products.js
+++ data/products.js
@@ -112,10 +112,13 @@ export function loadProductsFetch() {
     });
   
     console.log('load products');
+  }).catch(() => {
+    console.log('Unexpected error. Please try again later.');
   });
 
   return promise;
 }
+
 
 /*
 loadProductsFetch().then(() => {
@@ -139,9 +142,15 @@ export function loadProducts(fun) {
   fun();
   });
 
+  xhr.addEventListener('error', (error) => {
+    console.log('Unexpected error. Please try again later.');
+  });
+
   xhr.open('GET', 'https://supersimplebackend.dev/products');
   xhr.send();
 }
+
+
 
 
 /*
